ffea99b7db5c797cf47f78ec32fc7063c4833a72
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-08-06 00:33:39 -0700

    Users and Posts project in node/express

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+server.use(express.json());
+server.use(helmet());
+// server.use(morgan('common'));
+server.use(logger);
 
+//url = /api/users 👈💥
+//server.use('/api/users', verify('velcum!'), userRouter);
+server.use('/api/users', userRouter);
+server.get('/', auth, (req, res) =>{
+  res.status(200).send('Welcome users');
+});
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  req.name = req.body.name;
+  // console.log(`${req.name} made a function ${req.method} at ${req.url}`);
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+    'Origin'
+  )}`);
+  next();
+};
 
-module.exports = server;
+function verify(password) {
+  return function logger(req, res, next) {
+    if (req.headers.authorization === password) {
+      next();
+    } else {
+      res.status(404).json({ message: 'Please enter correct password' });
+    }
+  }
+};
+
+function auth(req, res, next) {
+  if(req.url === '/api/users') {
+    next();
+  } else {
+    res.send('Others are not allowed')
+  }
+};
+
+// module.exports = server;
+server.listen(4000, () => {
+  console.log('\n* Server Running on http://localhost:4000 *\n');
+});
